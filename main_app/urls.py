1173f427698ccf004acfd21204494e9d600a28c3
reembarqawi <reeembarqawi@gmail.com>
2025-10-11 14:30:19 +0300

    CRUD views for admin done

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,9 +5,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='product_list'),  # Home page
+    path('', ProductListView.as_view(), name='product_list'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('admin/products/add/', ProductCreateView.as_view(), name='product_add'),
-    path('admin/products/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_edit'),
-    path('admin/products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+    path('product/add/', ProductCreateView.as_view(), name='product_add'),
+    path('product/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_edit'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
